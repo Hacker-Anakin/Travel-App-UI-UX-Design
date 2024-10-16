4c595c9009f4eeb550d26132d2f1db1a29fb107b
Anmol <cakely63@gmail.com>
2024-10-16 09:38:21 +0530

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Travel",
+  description: "Travel UI-UX Website",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Navbar />
+        <main className="relative overflow-hidden">{children}</main>
+        <Footer/>
+      </body>
+    </html>
+  );
+}
